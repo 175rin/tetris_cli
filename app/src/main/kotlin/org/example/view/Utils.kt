3d5bfa163b75ead9621182376873d8cf5415c6a0
sota.morita <s.morisou@outlook.jp>
2026-07-05 01:47:14 +0900

    [add]TetrisのViewを作成

--- app/src/main/kotlin/org/example/view/Utils.kt
+++ app/src/main/kotlin/org/example/view/Utils.kt
@@ -1,16 +1,25 @@
 package org.example.view
 
-class Utils {
+object Utils {
+    /**
+     * カーソルを非表示
+     */
     fun hideCursor() {
         print("\u001B[?25l")
     }
 
+    /**
+     * カーソルを表示
+     */
     fun showCursor() {
         print("\u001B[?25h")
     }
 
+    /**
+     * 画面をリセットし、カーソルを左上に移動
+     */
     fun resetScreen() {
         print("\u001B[2J")
-        print("\u001B[H") // カーソルを左上に移動
+        print("\u001B[H") 
     }
 }
